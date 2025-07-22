78eb3eac62f50d3c3cd92e97fc1309f152e2c926
AxelIvanArroyoLara <arroyolaraaxelivan@gmail.com>
2025-07-21 21:18:01 -0600

    Trying to add as normal folder x3

--- src/sistema_almacen_deployment/js/script-disable_auto_completion.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('form').forEach(form => {
-        form.setAttribute('autocomplete', 'off');
-    });
-    document.querySelectorAll('input').forEach(input => {
-    input.setAttribute('autocomplete', 'off');
-    });
-});
